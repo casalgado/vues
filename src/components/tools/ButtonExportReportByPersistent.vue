57de064749fe086e38c5ff5e685e6f73dea0046a
casalgado <casalgado86@gmail.com>
2022-01-16 15:33:51 -0500

    improved client history report export

--- src/components/tools/ButtonExportReportByPersistent.vue
+++ src/components/tools/ButtonExportReportByPersistent.vue
@@ -7,21 +7,30 @@
     >
       <p>Exportar por Persistente</p>
     </download-csv>
+    <div @click="populateClientList">preparar reporte</div>
+    {{ clientsWithInfo.length > 0 ? "reporte listo" : "reporte no preparado" }}
   </div>
 </template>
 
 <script>
+import { ref } from "@/firebaseInit";
+import { getOneWhere } from "@/firebaseMethods";
 export default {
   name: "ButtonExportReportByPersistent",
   props: {
     orderedClients: Array,
     tables: Array,
   },
+  data() {
+    return {
+      clientsWithInfo: [],
+    };
+  },
   computed: {
     formattedObjects: function () {
       let forExport = [];
-      for (let i = 0; i < this.orderedClients.length; i++) {
-        const client = this.orderedClients[i];
+      for (let i = 0; i < this.clientsWithInfo.length; i++) {
+        const client = this.clientsWithInfo[i].name;
         let numberOfOrders = 0;
         for (let j = 0; j < this.tables.length; j++) {
           const tableItems = this.tables[j].clients;
@@ -32,9 +41,52 @@ export default {
             }
           }
         }
-        forExport.push({ client: client, numberOfOrders: numberOfOrders });
+        forExport.push({
+          numberOfOrders: numberOfOrders,
+          client: this.clientsWithInfo[i].name,
+          phone: this.clientsWithInfo[i].phone,
+          lastOrderDate: this.clientsWithInfo[i].lastOrderDate,
+          lastOrder: this.clientsWithInfo[i].lastOrder,
+        });
       }
       return forExport;
+    },
+  },
+  methods: {
+    getClientInfo: function (name) {
+      getOneWhere(ref, "clients", "name", name).then((e) => {
+        if (e) {
+          let lastOrder = [];
+          let lastOrderDate = "";
+          if (e.history) {
+            e.history[Object.keys(e.history).at(-1)].products.forEach((e) => {
+              lastOrder.push(e.quantity);
+              lastOrder.push(e.name);
+            });
+            lastOrderDate = e.history[Object.keys(e.history).at(-1)].date;
+          } else {
+            console.log(e.name);
+          }
+
+          this.clientsWithInfo.push({
+            name: e.name,
+            lastOrder: lastOrder.join(" "),
+            lastOrderDate: lastOrderDate.split("T")[0],
+            phone: e.phone,
+          });
+        } else {
+          this.clientsWithInfo.push({
+            name: name,
+            lastOrder: "",
+            phone: "",
+          });
+        }
+      });
+    },
+    populateClientList: function () {
+      for (let i = 0; i < this.orderedClients.length; i++) {
+        this.getClientInfo(this.orderedClients[i]);
+      }
     },
   },
 };
